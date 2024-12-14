0b6e313acb6261b338b49592ea5e3597252a1c8b
Mhmd Zmeter <mhmdz@Mhmds-MacBook-Pro.local>
2024-12-14 02:22:59 +0200

    migrate tables,make validation, create CRUD

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\storeRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $task = Task::all();
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(storeRequest $request)
+    {
+        $task = Task::create($request->validated());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        $task = Task::find($id);
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(storeRequest $request, int $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->validated());
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
